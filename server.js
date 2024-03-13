070c83da6ca76ee05afca62004ce20406b3b2a90
Bruno Mazzocchi <bruno.mazzocchi@tizo.app>
2024-03-13 11:46:58 -0600

    Replaced connection method for connection pool

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const mysql = require("mysql");
 
-// Crear una conexión a la base de datos
-const connection = mysql.createConnection({
+// Crear un pool de conexiones a la base de datos
+const pool = mysql.createPool({
+  connectionLimit: 10,
   host: "localhost",
   user: "root",
-  password: "Temporal2021+",
+  password: "strong_password",
   database: "persona",
+  port: 33060,
 });
 
 // Establecer la conexión a la base de datos
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Error al conectar a la base de datos: ", err);
     return;
   }
   console.log("Conexión exitosa a la base de datos");
+  connection.release(); // Liberar la conexión
 });
 
 // Crear una instancia de la aplicación Express
@@ -25,18 +28,31 @@ const app = express();
 app.get("/personas", (req, res) => {
   const query = "SELECT * FROM persona";
 
-  // Ejecutar la consulta a la base de datos
-  connection.query(query, (err, results) => {
+  // Obtener una conexión del pool
+  pool.getConnection((err, connection) => {
     if (err) {
-      console.error("Error al ejecutar la consulta: ", err);
+      console.error("Error al obtener una conexión del pool: ", err);
       res
         .status(500)
         .json({ error: "Error al obtener los datos de la tabla persona" });
       return;
     }
 
-    // Enviar los resultados en formato JSON al navegador web
-    res.json(results);
+    // Ejecutar la consulta a la base de datos
+    connection.query(query, (err, results) => {
+      connection.release(); // Liberar la conexión
+
+      if (err) {
+        console.error("Error al ejecutar la consulta: ", err);
+        res
+          .status(500)
+          .json({ error: "Error al obtener los datos de la tabla persona" });
+        return;
+      }
+
+      // Enviar los resultados en formato JSON al navegador web
+      res.json(results);
+    });
   });
 });
 
